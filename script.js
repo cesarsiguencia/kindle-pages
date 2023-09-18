7bb0da9a51fbfd0014e8026a3c85780b3c779adf
cesarsiguencia <cesar.siguencia@gmail.com>
2023-09-18 00:54:55 -0400

    first test for kindle

---
+++ script.js
@@ -0,0 +1,20 @@
+var time = 0 
+
+var initiate = function(){
+    time = time+1;
+    document.getElementById('time').innerText = time
+}
+
+setInterval(initiate, 1000)
+
+
+
+
+
+// var body = document.querySelector('.body')
+
+// var changeColor = function(){
+//     body.style.opacity = 0
+// }
+
+// document.addEventListener('click', changeColor)
