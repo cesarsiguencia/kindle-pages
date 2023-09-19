5b158456d6f9d2a848b507111360254934b1fe9f
cesarsiguencia <cesar.siguencia@gmail.com>
2023-09-19 12:30:53 -0400

    styled css for kindle display

--- script.js
+++ script.js
@@ -1,71 +1,40 @@
-// var time = 0 
-
-// var initiate = function(){
-//     time = time+1
-//     document.getElementById('time').innerText = time
-// }
-
-// setInterval(initiate, 1000)
-
-
-
-
-
 var body = document.querySelector('.body')
 
-var changeColor = function(){
-    body.style.backgroundColor = 'green'
-}
+var fetchQuotes = async function () {
 
 
-// var fetchQuotes = function(){
-
-//     var start = async function(){
-//         var res = await fetch('https://api.api-ninjas.com/v1/quotes?category=imagination', {
-//         method: 'GET',
-//         headers:{ 'X-Api-Key': 'l8MFF+UcPr3CLfTvYl3GQA==vqjL9aCZxpwhBPeE'},
-//         contentType: 'application/json',
-//     })
-    
-//     if(res.ok){
-//         var data= await res.json()
-//         console.log(data)
-//         document.querySelector('#quote').textContent = data[0].quote
-        
-//         document.querySelector('#name').textContent = data[0].author
-//     } else {
-//         document.querySelector('#quote').innerHTML = 'No quotes today!'
-//     }
-
-//     }
-//     start()
-    
-// }
+    var key = 'l8MFF+UcPr3CLfTvYl3GQA==vqjL9aCZxpwhBPeE';
+    var url = 'https://api.api-ninjas.com/v1/quotes?category=imagination';
 
 
-var fetchQuotes = async function(){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.overrideMimeType("application/json")
+    xmlHttp.open("GET", url, false);
+    xmlHttp.setRequestHeader("X-Api-Key", key);
+    xmlHttp.send(null);
+    console.log(xmlHttp.response)
+
+    var data = JSON.parse(xmlHttp.response)
+    console.log(data)
 
 
-        var res = await fetch('https://api.api-ninjas.com/v1/quotes?category=imagination', {
-        method: 'GET',
-        headers:{ 'X-Api-Key': 'l8MFF+UcPr3CLfTvYl3GQA==vqjL9aCZxpwhBPeE'},
-        contentType: 'application/json',
-    })
-    
-    if(res.ok){
-        var data= await res.json()
-        console.log(data)
+    if (xmlHttp.responseText) {
         document.querySelector('#quote').textContent = data[0].quote
-        
         document.querySelector('#name').textContent = data[0].author
     } else {
         document.querySelector('#quote').innerHTML = 'No quotes today!'
     }
 
+    setTimeout(function(){
+        location.reload()
+    }, 3600000)
 
-    
 }
 
-// setInterval(fetchQuotes(), 20000)
-window.onload = fetchQuotes
-document.addEventListener('click', changeColor)
+fetchQuotes()
+
+
+// function hello (){
+//     location.reload
+// }
+
