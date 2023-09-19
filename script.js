3c205d3eba19629bd1d177f6708876c2ca837f18
cesarsiguencia <cesar.siguencia@gmail.com>
2023-09-19 17:07:31 -0400

    final font settings with resizing of fonts done by js

--- script.js
+++ script.js
@@ -15,16 +15,28 @@ var fetchQuotes = function () {
 
 
     if (xmlHttp.responseText) {
-        document.querySelector('#quote').textContent = data[0].quote
+        // document.querySelector('#quote').textContent = data[0].quote
         document.querySelector('#name').textContent = data[0].author
     } else {
         document.querySelector('#quote').innerHTML = 'No quotes today!'
     }
 
+    fontResize()
     setTimeout(function(){
         location.reload()
     }, 3600000)
+    
+}
 
+var fontResize = function(){
+    var subBody = document.querySelector('.sub-div')
+
+    if(subBody.clientHeight > 1030){
+        console.log(subBody.clientHeight)
+        console.log('the body is too big')
+        document.querySelector('#quote').style.fontSize= '50px'
+        document.querySelector('#name').style.fontSize = '40px'
+    }
 }
 
 fetchQuotes()
